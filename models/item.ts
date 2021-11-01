fc16820fbbbc3a779059f698bf041a1e63199fd3
Matheus Caetano <matheuseprofissional@gmail.com>
2021-11-01 17:10:08 -0300

    main

--- models/item.ts
+++ models/item.ts
@@ -1,21 +1,22 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model, connect, ObjectId } from 'mongoose';
+
+const url = "mongodb://matheuscaet:mongopassword20092021@cluster0-shard-00-00.cfpvj.mongodb.net:27017,cluster0-shard-00-01.cfpvj.mongodb.net:27017,cluster0-shard-00-02.cfpvj.mongodb.net:27017/myFirstDatabase?authSource=admin&replicaSet=atlas-1023j9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
 
 // 1. Create an interface representing a document in MongoDB.
 interface iItem {
-  name: string;
-  email: string;
-  avatar?: string;
+    _id: {type: ObjectId, required: true},
+    name: string,
+    desc: string;
 }
 
 // 2. Create a Schema corresponding to the document interface.
-const schema = new Schema<User>({
+const schema = new Schema<iItem>({
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  avatar: String
+  desc: { type: String, required: true }
 });
 
 // 3. Create a Model.
-const UserModel = model<User>('User', schema);
+const ItemModel = model<iItem>('items', schema);
 
 run().catch(err => console.log(err));
 
@@ -23,12 +24,11 @@ async function run(): Promise<void> {
   // 4. Connect to MongoDB
   await connect('mongodb://localhost:27017/test');
 
-  const doc = new UserModel({
+  const doc = new ItemModel({
     name: 'Bill',
-    email: 'bill@initech.com',
-    avatar: 'https://i.imgur.com/dM7Thhn.png'
+    desc: 'bill@initech.com'
   });
   await doc.save();
 
-  console.log(doc.email); // 'bill@initech.com'
+  console.log(doc.name);
 }
