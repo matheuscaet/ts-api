19b84b56826183766ab5d39ca66aa3272b2b9414
MatheusCaet <matheuseprofissional@gmai.com>
2022-04-09 15:10:29 -0300

    fix: removing env data

--- src/config/env/app.env.ts
+++ src/config/env/app.env.ts
@@ -1,5 +1,6 @@
+require('dotenv').config({ path: '.env' });
 export const App = {
-    PORT: parseInt(process.env.PORT || "3333"),
-    URLMONGO: process.env.URLMONGO || "mongodb+srv://matheuscaet:mongopassword20092021@cluster0.cfpvj.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-1023j9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
-    NAMESPACE: process.env.NAMESPACE || "SERVER"
+    PORT: parseInt(process.env.PORT as string),
+    URLMONGO: process.env.URLMONGO as string,
+    NAMESPACE: process.env.NAMESPACE as string,
 }
